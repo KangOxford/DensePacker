96fe7526224206a521ae74286b16acd92e7593d5
Andrew <72123149+J-L-Andrew@users.noreply.github.com>
2022-08-13 20:56:49 +0800

    Add files via upload

---
+++ DP_torch/DDPG_runner.py
@@ -0,0 +1,147 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+import sys
+import copy
+import numpy as np
+import gym
+from utils import *
+from gym import spaces
+from gym.utils import seeding
+from collections import deque
+
+import sys
+import profile
+from utils import *
+import datetime
+import numpy as np
+
+
+from packing.scenario import Scenario
+
+
+from packing.cell.cell_gym import CellEnv
+# from particle_gym import MultiParticleEnv
+
+from model.pure_ddpg.ddpg import DDPGAgent
+import matplotlib.pyplot as plt
+
+def train_env(env_gym):
+
+    
+    scores, episodes = [], []
+    EPISODES = 2000
+    env_gym.agent.training = True
+    # 此处获得颗粒的状态
+    pass
+    for e in range(EPISODES):
+        done = False
+        
+        np.random.seed(0)
+        # 初始化cell，1.方向保留，长度重算， 2.直接回到最原始的立方体晶格
+        state = env_gym.reset()
+        score = 0
+        while not done:
+            # fresh env
+            env_gym.global_step += 1
+
+            # RL choose action based on observation and go one step
+            action = env_gym.get_agent_action(state)
+            next_state, reward, done, new_pos, noise, current_pos_onehot, potential_pos_onehot = env_gym.agent_step(
+                action)
+            # action_vector = np.zeros((env_gym.agent_action_size,))
+            # action_vector[action] = 1
+            env_gym.memory([noise, current_pos_onehot, potential_pos_onehot], new_pos, reward)
+            score += reward
+            state = next_state
+
+            if done and ((e + 1) % 10 == 0 or e == EPISODES-1):
+                probs = env_gym.train_episodes()
+                scores.append(score)
+                episodes.append(e)
+                print("episode:", e, "  score:", score, "  time_step:", env_gym.global_step)
+
+        if np.max(probs) > 0.75:
+            break
+        if len(scores) > 3:
+            if np.mean(scores[-3:]) <= -150:
+                break
+
+        if e % 100 == 0:
+            pass
+            env_gym.save_model("./models/transition_gradient.h5")
+
+def main():
+    
+    scenario = Scenario()
+    packing = scenario.build_packing()
+
+    # Create environment
+    agent_env = CellEnv(packing, scenario.reset_packing, scenario.reward, scenario.observation, scenario.done, method="rotation")
+    state_dim = [len(agent_env.observation_space.low)]
+    action_dim = [len(agent_env.action_space.low)]
+    action_boundaries = [agent_env.action_space.low, agent_env.action_space.high]
+
+    # Create agent and multi-particles
+    agent = DDPGAgent(state_dim, action_dim, action_boundaries, actor_lr = 1e-5,
+                     critic_lr = 1e-4, batch_size = 128, noise = 'ou', gamma = 0.99, rand_steps = 2,
+                     buffer_size = int(1e6), tau = 0.001)
+
+    np.random.seed(0)
+    PD = []
+    Penalty = []
+
+    #training loop: call remember on predicted states and train the models
+    episode = 0
+    for i in range(3000):
+        #get initial state
+        state = agent_env.reset()
+        if i == 500:
+            filename = f'{packing.fraction:.3f}_zero.scr'
+            scr(filename, 'sphere', packing.visable_particles, packing.agent.state.lattice)
+        terminal = False
+        score = 0
+        step = 0
+        #proceed until reaching an exit state
+        while not terminal:
+            #predict new action
+            action = agent.get_action(state, episode)
+            #print(action)
+            #perform the transition according to the predicted action
+            state_new, reward, terminal, info = agent_env.step(action)
+            #store the transaction in the memory
+            agent.remember(state, state_new, action, reward, terminal)
+            #adjust the weights according to the new transaction
+            agent.learn()
+            #iterate to the next state
+            state = state_new
+            score += reward
+            step += 1
+            #env.render()
+        #if packing.cell_penalty == 0. and packing.fraction > 0.5:
+            if i == 500:
+                filename = f'{packing.fraction:.3f}.scr'
+                scr(filename, 'sphere', packing.visable_particles, packing.cell.state.lattice)
+        
+        if i == 500:
+                filename = f'{packing.fraction:.3f}.scr'
+                scr(filename, 'sphere', packing.visable_particles, packing.cell.state.lattice)
+
+
+        PD.append(packing.fraction)
+        Penalty.append(packing.cell_penalty)
+        print("Iteration {:d} --> step {:d} score {:.2f}. packing density {:.4f}".format( i, step, score, packing.fraction))
+        print("penalty {:2f} elite_volume {:2f}".format(packing.cell_penalty, packing.cell.volume_elite))
+        episode += 1
+
+    plt.figure(figsize=[8,6])
+    plt.plot(PD, 'black',linewidth = 3.0)
+    plt.plot(Penalty,'black',ls='--',linewidth = 3.0)
+    plt.legend(['PD' , 'Penalty'],fontsize=18)
+    plt.xlabel('Episode' ,fontsize=16)
+    plt.ylabel('Goal',fontsize=16)
+    plt.title('Loss Curves',fontsize=16)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ DP_torch/utils.py
@@ -0,0 +1,169 @@
+import numpy as np
+
+import torch
+from pytorch3d import transforms
+from scipy import optimize
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+def Heaviside(x):
+    """
+    step function
+    """
+    if x > 0: return 1
+    else: return 0
+
+def abs_norm(x, Relu=False):
+    temp = relu(x) if Relu else x
+    return int(np.linalg.norm(temp, ord=1))
+
+
+class Transform():
+    def __init__(self):
+        pass
+
+    def euler2mat(self, angle):
+        """
+        Convert rotations given as Euler angles in radians to rotation matrices.
+        """
+        x = torch.as_tensor(angle, dtype=np.double)
+        mat = transforms.euler_angles_to_matrix(x)
+        return mat
+    
+    def euler2qua(self, angle):
+        """
+        Convert euler angle to quaternion
+        """
+        x = self.euler2mat(angle)
+        qua = transforms.matrix_to_quaternion(x)
+        return qua
+
+def data_scale(unscaled, from_range, to_range):
+    x = (unscaled - from_range[0]) / (from_range[1] - from_range[0])
+    x = x * (to_range[1] - to_range[0]) + to_range[0]
+    return x
+
+
+def surface_area(lattice):
+    """
+    Calculate the surface area for simulation cell
+    """
+    area = 0.
+    for i in range(3):
+        direction = [0, 1, 2]
+        direction.remove(i)
+
+        [d1, d2] = direction
+        area += 2.*np.linalg.norm(np.cross(lattice[d1], lattice[d2]))
+    return area
+
+def scr(filename, type, particles, lattice):
+
+	with open(filename, 'w') as f:
+            f.write('-osnap off\n')
+            f.write("erase all \n")
+            f.write("vscurrent 2\n")
+
+            if type == 'sphere':
+                for i in range(len(particles)):
+                    for a, p in enumerate(particles[i]):
+                        center = p.state.centroid
+                        radius = p.radius
+                        f.write(f'sphere {center[0]},{center[1]},{center[2]} {radius}\n')
+            
+                o = np.zeros(3)
+                for k in range(len(particles[0])):
+                    o += particles[0][k].state.centroid / len(particles[0])
+            
+                direction = [0, 1, 2]
+                for j in range(3):
+
+                    vector = lattice[j]
+                    direction = [0, 1, 2]
+                    direction.remove(j)
+
+                    [d1, d2] = direction
+                    for m in range(2):
+                        for n in range(2):
+                            origin = o + m*lattice[d1] + n*lattice[d2]
+                            axis = origin + vector
+                            f.write(f'line {origin[0]},{origin[1]},{origin[2]} {axis[0]},{axis[1]},{axis[2]} \n')
+            
+                f.write("zoom e ")   
+
+
+def scaled_coordinate(position, frame):
+    """
+    frame = [v1 v2 v3] (Column-based Storage)
+    """
+    temp = np.linalg.pinv(frame)
+    new_pos = np.matmul(temp, position.T).T
+    return new_pos
+
+
+# overlap potential
+def overlap_fun(type, particle_a, particle_b):
+    """
+    calculate overlap potential (energy) between two particles
+    """
+    r_AB = particle_b.state.centroid - particle_a.state.centroid
+
+    if type in ('ellipsoid', 'ellipse'):
+        # maximum of PW function Fun_AB
+        X_A = particle_a.char_mat
+        X_B = particle_b.char_mat
+
+        t_c = optimize.fminbound(lambda t: - Fun_AB(t, X_A, X_B, r_AB), 0, 1)
+        f = Fun_AB(t_c, X_A, X_B, r_AB)
+        overlap_p = 0.5 * Heaviside(-f) * f**2
+
+    elif type in ('sphere', 'disk'):
+        r = np.linalg.norm(r_AB)
+        sigma = particle_a.radius + particle_b.radius
+        x = 1. - r / sigma
+        overlap_p = 0.5 * Heaviside(x) * x**2
+
+    return overlap_p
+
+def Fun_AB(t, XA, XB, r):
+    """
+    Calculation of Perram-Wertheim function:
+    F_AB = t(1-t)r^T Y^{-1} r - 1, where Y = t*XB^{-1}+(1-t)*XA^{-1}
+    """
+    Y = t*np.linalg.pinv(XB) + (1.-t)*np.linalg.pinv(XA)
+    Y = np.linalg.pinv(Y)
+
+    F_AB = np.matmul(r.reshape(1,-1), Y)
+    F_AB = t*(1.-t)*np.matmul(F_AB, r.reshape(-1,1)) - 1.
+
+    assert len(F_AB) == 1
+    return F_AB[0][0]
+
+
+
+
+
+def output_xyz(filename, packing):
+    """
+    For visulaization in ovito
+    """
+    centroid = [particle.centroid for particle in packing.visable_particles]
+    quaternion = [Transform.euler2qua(particle.orientation) for particle in packing.visable_particles]
+    semi_axis = [particle.semi_axis for particle in packing.visable_particles]
+    color = [particle.color for particle in packing.visable_particles]
+
+    # colors = self.color_palette(labels, probabilities)
+    n = len(packing.visable_particles)
+    with open(filename, 'w') as f:
+        # The keys should be strings
+        f.write(str(n) + '\n')
+        f.write('Lattice="' + ' '.join([str(vector) for vector in packing.cell.lattice.flat]) + '" ')
+        # f.write('Origin="' + ' '.join(str(index) for index in packing.cell.origin) + '" ')
+        f.write('Properties=pos:R:3:orientation:R:4:aspherical_shape:R:3:color:R:3 \n')
+
+        if (packing.particle_type == 'ellipsoid'):
+            np.savetxt(f, np.column_stack([centroid, quaternion, semi_axis, color]))
+
+
