0909bb589264b681154e1f62bdba74113327697c
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-14 10:22:40 +0000

    train

---
+++ DP_torch/packing/cell/train_cell_gym.py
@@ -0,0 +1,25 @@
+# import gym
+# from stable_baselines3.common.vec_env import DummyVecEnv
+# env_id = 'DensePacking-v1'
+# env = DummyVecEnv([lambda: gym.make(env_id)])
+
+
+from stable_baselines3 import PPO
+
+from packing.scenario import Scenario
+from packing.cell.cell_gym import CellEnv
+
+
+scenario = Scenario()
+    packing = scenario.build_packing()
+    # Create environment
+    env = CellEnv(packing, scenario.reset_packing, scenario.reward, scenario.observation, scenario.done)
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=25000)
+    model.save("ppo_densepacking")
+
+    obs = env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
