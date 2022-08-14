b90b16d1906a4cf8caa462973229ae018d07a0f4
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-14 11:55:37 +0000

    new

--- DP_torch/packing/cell/train_cell_gym.py
+++ DP_torch/packing/cell/train_cell_gym.py
@@ -11,15 +11,15 @@ from packing.cell.cell_gym import CellEnv
 
 
 scenario = Scenario()
-    packing = scenario.build_packing()
-    # Create environment
-    env = CellEnv(packing, scenario.reset_packing, scenario.reward, scenario.observation, scenario.done)
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=25000)
-    model.save("ppo_densepacking")
+packing = scenario.build_packing()
+# Create environment
+env = CellEnv(packing, scenario.reset_packing, scenario.reward, scenario.observation, scenario.done)
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("ppo_densepacking")
 
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        env.render()
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
