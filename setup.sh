b3bc0bfb161816a413c833c64ccfdd9f97aa4ad7
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-14 09:49:15 +0000

    new

---
+++ setup.sh
@@ -0,0 +1,16 @@
+export PATH=/workspace/anaconda3/bin:$PATH
+export PATH=/workspace/DensePacking/DP_torch:$PATH
+
+conda activate dp
+
+##python version == 3.7.3
+# pip install torch==1.11.0
+# pip install ipython
+# pip install keras
+# pip install gym
+# pip install pytorch3d
+# pip install tk
+# pip install scipy
+
+
+
