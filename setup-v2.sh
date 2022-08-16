a717eb1bf1f6922e50f8936c97d51ff4548210c8
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-16 14:11:04 +0000

    first

--- setup-v2.sh
+++ setup-v2.sh
@@ -11,6 +11,7 @@ pip install tk
 pip install scipy
 pip install stable-baselines3
 pip install ipython
+pip install tensorflow
 pip install tensorboard
 
 export PYTHONPATH=/workspace/DensePacking/DP_torch:$PYTHONPATH
@@ -19,4 +20,11 @@ export PYTHONPATH=/workspace/DensePacking/DP_torch:$PYTHONPATH
 # python -m cProfile -s cumulative /workspace/DensePacking/DP_torch/train_cell_gym.py > analysis_train_cell_gym.txt
 # python /workspace/DensePacking/DP_torch/train_cell_gym.py > log_train_cell_gym-v10.txt
 # python /workspace/DensePacking/DP_torch/train_cell_gym.py
-# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-v11/
+
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-3/
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-4/
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-5/
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-7/
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-8/
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-10/
+# tensorboard --logdir /workspace/DensePacking/ppo_densepacking_tensorboard-11/
